d16fb86f333144f9d3d0e2ffc627545121ac9c78
Akash <akash9122000@gmail.com>
2025-04-01 18:37:38 +0530

    work in progress

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from app.views import home, create_article
+# from app.views import home, create_article
+from app.views import ArticleCreateView, ArticleUpdateView, ArticleDeleteView, ArticleListView
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("article/create/", create_article, name="create_article")
+    # path("", home, name="home"),
+    # path("article/create/", create_article, name="create_article")
+    path('', ArticleListView.as_view(), name="home"),
+    path("article/create/", ArticleCreateView.as_view(), name="create_article"),
+    path("article/<int:pk>/update/", ArticleUpdateView.as_view(), name="update_article"),
+    path("article/<int:pk>/delete/", ArticleDeleteView.as_view(), name="delete_article")
 ]
