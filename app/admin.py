89f9f1d44e8ea2669310190e96a8aa0cdd80397f
psychoAKASH <akash9122000@gmail.com>
2025-04-13 13:56:42 +0530

    changes done in admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from app.models import Article, UserProfile
+from django.contrib.auth.admin import UserAdmin
 
 
 # Register your models here.
@@ -13,5 +14,22 @@ class ArticleAdmin(admin.ModelAdmin):
     readonly_fields = ('word_count', 'created_at', 'updated_at')
 
 
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permission")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")})
+    )
+    add_fieldsets = (
+        (None, {"classes": ("wide",),
+                "fields": ("email", "password1", "password2")}),
+    )
+    list_display = ("email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    search_field = ("email",)
+    ordering = ("email",)
+
+
 admin.site.register(Article, ArticleAdmin)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, CustomUserAdmin)
