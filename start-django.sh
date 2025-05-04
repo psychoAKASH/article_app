87bfd5e9fbdf2f0378300053a8530b1461cdfe4e
psychoAKASH <akash9122000@gmail.com>
2025-05-04 13:48:19 +0530

    rquired changes done necessary for the build process

--- start-django.sh
+++ start-django.sh
@@ -2,7 +2,7 @@
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-if [[ "$ENV_STATE" == "production"]]; then
+if [[ "$ENV_STATE" == "production" ]]; then
   gunicorn djproject.wsgi --workers 2 $GUNICORN_WORKERS --forwarded-allow-ips "*"
 else
   python manage.py runserver 0.0.0.0:8000
